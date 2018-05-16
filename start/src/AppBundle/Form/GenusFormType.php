21bd766f44473a465f08ff70cf87e3fd2d5e0524
abel verbo <havelinux@gmail.com>
2018-05-16 09:16:17 +0200

    render the form pretty

--- start/src/AppBundle/Form/GenusFormType.php
+++ start/src/AppBundle/Form/GenusFormType.php
@@ -3,21 +3,22 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GenusFormType extends AbstractType
 {
-
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
       ->add('name')
       ->add('speciesCount')
-      ->add('funFact');
+      ->add('funFact')
+    ;
   }
 
   public function configureOptions(OptionsResolver $resolver)
   {
 
   }
-
 }
