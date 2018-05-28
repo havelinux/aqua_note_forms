cc3009035ca96cb9096d4ceec253e43ea3a1242b
abel verbo <havelinux@gmail.com>
2018-05-28 08:59:49 +0200

    edit form

--- start/src/AppBundle/Entity/Genus.php
+++ start/src/AppBundle/Entity/Genus.php
@@ -66,6 +66,13 @@ class Genus
         $this->notes = new ArrayCollection();
     }
 
+    /**
+     * @return mixed
+     */
+    public function getId() {
+      return $this->id;
+    }
+
     public function getName()
     {
         return $this->name;
@@ -101,7 +108,7 @@ class Genus
 
     public function getFunFact()
     {
-        return '**TEST** '.$this->funFact;
+        return $this->funFact;
     }
 
     public function setFunFact($funFact)
